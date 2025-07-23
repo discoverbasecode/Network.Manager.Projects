df095bdadf546ba645e5dd1e13483aeb821a7a7b
Discoverbasecode <Discoverbasecode@gmail.com>
2025-07-23 22:06:47 +0330

    register all feature  in Main Method in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Net.NetworkInformation;
+using Spectre.Console;
+
+namespace Network.Manager.Console.App;
+
+abstract class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Title = "Network Manager";
+        UiComponent.DisplayWelcomeAnimation();
+
+        while (true)
+        {
+            UiComponent.DisplayMenu();
+            var choice = AnsiConsole.Prompt(
+                new TextPrompt<string>("[green]Select an option (1-6):[/]")
+                    .Validate(input => input switch
+                    {
+                        var x when string.IsNullOrWhiteSpace(x) => ValidationResult.Error(
+                            "[red]Please enter a number![/]"),
+                        var x when !int.TryParse(x, out int n) || n < 1 || n > 6 => ValidationResult.Error(
+                            "[red]Please enter a number between 1 and 6![/]"),
+                        _ => ValidationResult.Success()
+                    }));
+
+            switch (choice)
+            {
+                case "1":
+                    DnsAdapter.DisplayCurrentDNS();
+                    break;
+                case "2":
+                    DnsAdapter.SetNewDNS();
+                    break;
+                case "3":
+                    IpAdapter.DisplayCurrentIP();
+                    break;
+                case "4":
+                    IpAdapter.SetNewIP();
+                    break;
+                case "5":
+                    NetworkAdapters.ToggleNetworkAdapter();
+                    break;
+                case "6":
+                    UiComponent.ShowExitAnimation();
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+}
